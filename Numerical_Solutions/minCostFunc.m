fb240086d810dc3c893d21549cc7fbf244391bdc
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2017-10-31 23:09:46 -0400

    All-in-one reformulation
    Calculcates cost and policy

--- Numerical_Solutions/minCostFunc.m
+++ Numerical_Solutions/minCostFunc.m
@@ -1,6 +1,11 @@
-function [ minCostOut ] = minCostFunc( x,u )
-%minCostFunc Calculates min cost
+function [ minCostOut ] = minCostFunc2( x,u )
+%minCostFunc2 Calculates min cost, all-in-one
 
-minCostOut=x^2+u^2+minCostFunc(f(x,u), fminbnd(minCostCtrlFunc,-f(x,u),5-f(x,u)) );
+totalCtrlCost=@(u)(u^2 + minCostFunc2(  f(x,u), fminbnd(nextStateCost,-f(x,u),5-f(x,u)) ));
+nextStateCost=@(v) minCostFunc2(f(x,u),v);
+
+u0=fminbnd(totalCtrlCost,-x,5-x);
+
+minCostOut=x^2+u0^2+ minCostFunc2(  f(x,u), fminbnd(nextStateCost,-f(x,u),5-f(x,u)) );
 
 end
