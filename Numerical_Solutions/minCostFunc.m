60d5fe926c3933534ad31fc19afc5539f12a9b55
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2017-10-31 23:12:44 -0400

    Reformulation w states
    Added states, and pointer passing to avoid repeated recursion

--- Numerical_Solutions/minCostFunc.m
+++ Numerical_Solutions/minCostFunc.m
@@ -1,15 +1,28 @@
-function [ minCostOut ] = minCostFunc3( x )
-%minCostFunc3 Calculates min cost, all-in-one (version 2)
+function [ minCostOut ] = minCostFunc4( x,k,currStateCostPtr )
+%minCostFunc4 Calculates min cost, all-in-one (version 4)
 %   Cost calculation dependent only on x (u set dynamically)
-%   Issue: repetition of recursion, infinite loop
+%   Added state 
+%   Added pointer passing to avoid repeated recursion
+%   ERROR: fminbnd not returning
+
+nextStateCostPtr = RefValue; %Will hold cost of next state
+nextStateCostPtr.data = 0; %By default, no cost of next state
 
 f=@(x,u)x+u;
 
-totalCtrlCost=@(u)(u^2 + minCostFunc3(f(x,u)));
+if k<3
+    %In any but last state, find optimum ctrl
+    totalCtrlCost=@(u)(u^2 + minCostFunc4(f(x,u),k+1,nextStateCostPtr));
+    uOpt=fminbnd(totalCtrlCost,-x,5-x); %<--------- ERROR HERE
+else
+    %In last state, no control
+    uOpt=0;
+end
 
-uOpt=fminbnd(totalCtrlCost,-x,5-x);
-fprintf('uOpt=%f',uOpt)
+fprintf('State=%d, uOpt=%f',k,uOpt); %Display state #, optimal ctrl
 
-minCostOut=x^2+uOpt^2+ minCostFunc3(f(x,u));
+%Calculate total state cost
+currStateCostPtr.data=x^2+uOpt^2+nextStateCostPtr.data;
+minCostOut=currStateCostPtr.data;
 
 end
