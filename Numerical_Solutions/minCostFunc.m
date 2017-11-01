2fb6e619dab9acdf7c4c7cdd94652a17307354c4
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2017-10-31 23:15:26 -0400

    W states ptr
    Replaced index k with states ptr. Also, now only running for up to 2
    states (0-1), NOT 4.

--- Numerical_Solutions/minCostFunc.m
+++ Numerical_Solutions/minCostFunc.m
@@ -1,5 +1,5 @@
-function [ minCostOut ] = minCostFunc4( x,k,currStateCostPtr )
-%minCostFunc4 Calculates min cost, all-in-one (version 4)
+function [ minCostOut ] = minCostFunc5( x,currStatePtr,currStateCostPtr )
+%minCostFunc5 Calculates min cost, all-in-one (version 5)
 %   Cost calculation dependent only on x (u set dynamically)
 %   Added state 
 %   Added pointer passing to avoid repeated recursion
@@ -8,18 +8,21 @@ function [ minCostOut ] = minCostFunc4( x,k,currStateCostPtr )
 nextStateCostPtr = RefValue; %Will hold cost of next state
 nextStateCostPtr.data = 0; %By default, no cost of next state
 
+nextStatePtr = RefValue;
+nextStatePtr.data = currStatePtr.data+1;
+
 f=@(x,u)x+u;
 
-if k<3
+if currStatePtr.data<1
     %In any but last state, find optimum ctrl
-    totalCtrlCost=@(u)(u^2 + minCostFunc4(f(x,u),k+1,nextStateCostPtr));
-    uOpt=fminbnd(totalCtrlCost,-x,5-x); %<--------- ERROR HERE
+    totalCtrlCost=@(u)(minCostFunc5(f(x,u),nextStatePtr,nextStateCostPtr));
+    fminbnd(totalCtrlCost,-x,5-x); %<--------- ERROR HERE
 else
     %In last state, no control
     uOpt=0;
 end
 
-fprintf('State=%d, uOpt=%f',k,uOpt); %Display state #, optimal ctrl
+fprintf('State=%d, uOpt=%f',currStatePtr.data,uOpt); %Display state #, optimal ctrl
 
 %Calculate total state cost
 currStateCostPtr.data=x^2+uOpt^2+nextStateCostPtr.data;
