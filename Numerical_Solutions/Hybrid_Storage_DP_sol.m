3b7c8eb62587c39fbd79f09c8cb68fbdda332b0e
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2017-11-14 23:56:22 -0500

    Using corrected DP looping program

--- Numerical_Solutions/Hybrid_Storage_DP_sol.m
+++ Numerical_Solutions/Hybrid_Storage_DP_sol.m
@@ -1,56 +1,97 @@
+%warning('off', 'Octave:possible-matlab-short-circuit-operator');
+
 clear all;
 
-INF=1e6;
-MIN_STATE=-10;
-MAX_STATE=10;
+%Input: initial state
+x_init=3; %Must be between MIN_STATE and MAX_STATE
 
-MIN_PERTURB=-2;
-MAX_PERTURB=2;
+%ASSUMING FINAL COST = 0
+
+INF=1e6;
+MIN_STATE=1;
+MAX_STATE=3;
+
+MIN_PERTURB=0;
+MAX_PERTURB=0;
 P_PERTURB=1/(MAX_PERTURB-MIN_PERTURB+1);
 
 j=1; %storage device number
 ALPHA=[];
 BETA=[];
 
-global uOpt;
-global LAST_ITER;
 
 %Define ending iteration
-LAST_ITER=2;    %Recurse for 2 iterations (1 and 2)
+LAST_ITER=4;    %Recurse for 4 iterations (1,2,3,4)
 
-V(MIN_STATE:MAX_STATE,1:LAST_ITER) = INF;     %V(:,k) holds the cost of the kth iteration for each possible state
-uOptState(1:MAX_STATE-MIN_STATE+1)=0;         %uOptState holds the optimal control for each state, at a SINGLE GIVEN iteration
-uOpt(1:LAST_ITER)=0;                          %uOpt holds the optimal control for each iteration
-optIterCost(1:LAST_ITER)=INF;                 %holds lowest cost amongst all states AT a GIVEN iteration (i.e. state to choose)
 
-V(:,LAST_ITER)=0;                             %final cost is 0, for all possible states
+V(1:(MAX_STATE-MIN_STATE+1),1:LAST_ITER) = INF; %COST MATRIX....V(:,k) holds the cost of the kth iteration for each possible state
+uOptState(1:(MAX_STATE-MIN_STATE+1),1:LAST_ITER)=0;         %uOptState holds the optimal control for each state, and for all iterations
+uOpt(1:LAST_ITER)=0;                            %uOpt holds the optimal control for each iteration, starting from the GIVEN INITIAL STATE
+optState(1:LAST_ITER)=INF;                      %holds the value of the optimal state AT a GIVEN iteration
 
-for t=(LAST_ITER-1):1         %Start at 2nd-last iteration (time, t), and continue backwards
-  for x=MIN_STATE:MAX_STATE   %For each state at an iteration...
-    CostX(x)=INF;             %CostX will LOWEST cost-to-go for a state. (Assume infinite cost by default)
-    for u=-x:(-x+5)           %For each possible control for that state (at a given iteration)... (TO DO: customize set of possible control)
-      CostX_U=0;              %CostX_U will hold EXPECTED cost of next state
-      for w=MIN_PERTURB:MAX_PERTURB     %Find expected cost-to-go for a given control to be the Expected Cost for over all random perturbances
-        if((x+u+w)<MAX_STATE && (x+u+w)>MIN_STATE) %If next state is amongst those achievable with a given perturbance....
-          CostX_U=CostX_U+V(t+1,x+u+w)*P_PERTURB   %Add to running cost     (TO DO: customize next-state calculation)
+V(:,LAST_ITER)=0; %final cost is 0, for all possible states
+%V(:,LAST_ITER)=linspace(1,(MAX_STATE-MIN_STATE+1),13); %final cost is least for state -6
+
+%NOTE: at end, uOpt will have best control policy, and NetCost will contain total cost of DP operation
+
+for t=(LAST_ITER-1):-1:1         %Start at 2nd-last iteration (time, t), and continue backwards
+  for state_Index=1:(MAX_STATE-MIN_STATE+1)   %For each state at an iteration...
+    x=MIN_STATE+(state_Index-1);      %Map state index to state (TO DO: customize to non-consecutive AND/OR non-integer states)
+    CostX(state_Index)=INF;           %CostX will be LOWEST cost-to-go for a state. (Assume infinite cost by default)
+    
+    for u=(MIN_STATE-MAX_STATE):(MAX_STATE-MIN_STATE)     %For each possible control for that state (at a given iteration)... (TO DO: customize set of possible control)
+      CostX_U=0;                      %CostX_U will hold EXPECTED cost of next state, for GIVEN u.
+      
+      %Find cost-to-go for each possible control, under conditions of perturbations
+      %NOTE: this is the perturbation of the next state [w(k+1)]
+      for w=MIN_PERTURB:MAX_PERTURB   %Find expected cost-to-go for a given control to be the Expected Cost for over all random perturbances
+        if((x+u+w)<=MAX_STATE && (x+u+w)>=MIN_STATE) %If next state is amongst those achievable with a given perturbance....
+          nextState_Index=(x+u+w)-MIN_STATE+1;       %Map state to state index, to find cost of next state based on its index
+          CostX_U=CostX_U+V(nextState_Index,t+1)*P_PERTURB;   %Add to running cost     (TO DO: customize probability distribution)
+        else
+          %fprintf('Control not allowed: x=%d,u=%d,x+u=%d\n',x,u,x+u);
         end
       end
-      if(CostX_U=0) disp('State with no achievable next state') return end; %If cannot go to any next state, break script. (TO DO: update to cost=INF)
-      if(CostX(x)>CostX_U)    %Find lowest cost-to-go for the state amongst those possible for each of the controls
-        CostX(x)=CostX_U;
-        uOptState(x)=u;       %Find best control for the state (providing the lowst cost to go)
+      %NOTE: IF NO PERTURBATION.... CostX_U should just hold cost of next state for the given value of u.
+      if(CostX_U==0 && (t~=LAST_ITER-1)) %If cannot go to any next state FOR GIVEN CONTROL, update to cost=INF. Exception: in penultimate iteration (ASSUMING final cost=0)
+        %ASSUMPTION: all of next possible states have non-zero cost, except for last iteration
+        %disp('No achievable next state for this control');
+        CostX_U = INF;
+      end
+      
+      
+      %Find the Cost-To-Go+Control combination yielding lowest cost for state, amongst those possible for each admissible value of control
+      if( (CostX(state_Index)+uOptState(state_Index,t)^2) > (CostX_U+u^2) )  %MOST IMPORTANT
+        % (TO DO: customize current control cost calculation)
+        CostX(state_Index)=CostX_U;     %For best combo, set cost-to-go to be that cost-to-go for the state
+        uOptState(state_Index,t)=u;     %For best combo, set control to give that combo
       end
     end
-    V(t,x)=CostX(x) + (x^2+uOptState(x)^2);  %Fill in value vector (for a given time t) with: LOWEST costs-to-go for each state + Cost of the state ITSELF (x^2+u^2)
+    
+    %Set next state costs in Cost Matrix
+    if(CostX(state_Index)==INF)
+      disp('State with no achievable next state');
+    end    
+    V(state_Index,t)=CostX(state_Index) + (x^2+uOptState(state_Index,t)^2);  %Fill in value vector (for a given time t) with: LOWEST costs-to-go for each state + Cost of the state ITSELF (x^2+u^2)
     % (TO DO: customize current state cost calculation)
-    if(optIterCost(t)>V(t,x)) %Find lowest cost amongst all the states at a given time
-      optIterCost(t)= V(t,x);
-      uOpt(t)=uOptState(x);   %Optimal control at an iteration amongst all the states is that which has the lowest cost of state.
   end
 end
 
-function [ nextX ] = StateEqn( x,u,w,1 )
-  nextX(1)=BETA(1)*x(1)+ALPHA_C(1)*uC(1)-1/ALPHA_D(1)*uD(1)
+
+%Get index associated with initial state
+initStateInd = x_init-MIN_STATE+1;
+%Get minimum total cost given that starting state
+NetCost=V(initStateInd,1);
+%Get control policy
+state_Index=initStateInd; %Starting from initial state...
+for(t=1:(LAST_ITER)) %Iterate through cost matrix to find optimal control values
+  uOpt(t)=uOptState(state_Index,t);       % Store in uOpt
+  optState(t)= MIN_STATE+(state_Index-1); % Also store optimal subsequent states (for reference)
+  state_Index = state_Index+uOpt(t);      %(TO DO: ADD EFFECT OF PERTURBATION IN CURRENT STATE)
 end
 
-%NOTE: at end, uOpt will have best control policy, and optIterCost(1) will contain total cost of DP operation
+
+
+function [ nextX ] = StateEqn( x,u,w,j )
+  nextX(j)=BETA(j)*x(1)+ALPHA_C(j)*uC(1)-1/ALPHA_D(j)*uD(j)
+end
