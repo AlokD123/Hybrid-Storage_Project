2e4c98662bbd46b51723e1cc74b24f5f155ff392
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2017-12-05 16:11:56 -0500

    Added C_MAX and C_MIN conditions
    Conditions:
    1) net supply (discharging) never below demand, 2) not charging cap. too
    quickly

--- Numerical_Solutions/Hybrid_Storage_DP_sol.m
+++ Numerical_Solutions/Hybrid_Storage_DP_sol.m
@@ -20,7 +20,7 @@ LAST_ITER=3;
 
 %Model setup
 
-MAX_CHARGE=[0;100]; %Maximum charging of the supercapacitor
+MAX_CHARGE=[0;100]; %Maximum charging of the 1) battery and 2) supercap
 MAX_DISCHARGE=[2;1]; %Maximum discharging of the 1) battery and 2) supercap
 
 MIN_LOAD=0; %Minimum load expected
@@ -80,29 +80,34 @@ for t=(LAST_ITER-1):-1:1                %Start at 2nd-last iteration (time, t), 
         %For each possible control for that state (at a given iteration and value of w)...
         for D1=0:MAX_DISCHARGE(1)
           for D2=0:MAX_DISCHARGE(2)
-            fprintf('D2=%d\n',D2);
             %If next state is amongst those achievable with a given perturbance....
             if(StateEqn1(E1,D1)<=E_MAX(1) && StateEqn1(E1,D1)>=E_MIN(1))
               if(StateEqn2(E2,D1,D2,L)<=E_MAX(2) && StateEqn2(E2,D1,D2,L)>=E_MIN(2))
-                %Map state to state index, to find cost of next state based on its index
-                nextE_Ind1=round(StateEqn1(E1,D1)-E_MIN(1)+1);
-                nextE_Ind2=round(StateEqn2(E2,D1,D2,L)-E_MIN(2)+1); 
-                
-                %%%% MISSING CONDITION C_MAX... add here
-                
-                %Find the Cost-To-Go+Control combination yielding lowest cost for state, amongst those possible for each admissible value of control
-                %MOST IMPORTANT: minimization
-                if( (CostE1_L(indL)+CtrlCost(D1Opt_State(E_Ind1,t),D2Opt_State(E_Ind2,t),L)) > (V1(nextE_Ind1,t+1)+CtrlCost(D1,D2,L)) )
-                  if( (CostE2_L(indL)+CtrlCost(D1Opt_State(E_Ind1,t),D2Opt_State(E_Ind2,t),L)) > (V2(nextE_Ind2,t+1)+CtrlCost(D1,D2,L)) )
-                    %Note: Cost-to-go for given u&w (state at next time) is that at time t+1 in matrix V
-                    
-                    %For best combo, set cost-to-go to be that cost-to-go for the state
-                    CostE1_L(indL)=V1(nextE_Ind1,t+1); 
-                    CostE2_L(indL)=V2(nextE_Ind2,t+1); 
-                    %For best combo, set control to give that combo
-                    D1Opt_State(E_Ind1,t)=D1;  
-                    D2Opt_State(E_Ind2,t)=D2;  
-                    %^^ value of uOptState obtained here is UNUSED after value of CostX_W is found!!!
+              
+                %IF meeting following conditions:
+                %1)net supply (discharging) never below demand, 2) not charging cap. too quickly
+                if(~((D1+D2-L)<0||(D1+D2-L)>MAX_CHARGE(2)))
+                  %Map state to state index, to find cost of next state based on its index
+                  nextE_Ind1=round(StateEqn1(E1,D1)-E_MIN(1)+1);
+                  nextE_Ind2=round(StateEqn2(E2,D1,D2,L)-E_MIN(2)+1); 
+                  
+                  %%%% MISSING CONDITIONS for C_MAX and C_MIN... add here
+                  
+                  
+                  %Find the Cost-To-Go+Control combination yielding lowest cost for state, amongst those possible for each admissible value of control
+                  %MOST IMPORTANT: minimization
+                  if( (CostE1_L(indL)+CtrlCost(D1Opt_State(E_Ind1,t),D2Opt_State(E_Ind2,t),L)) > (V1(nextE_Ind1,t+1)+CtrlCost(D1,D2,L)) )
+                    if( (CostE2_L(indL)+CtrlCost(D1Opt_State(E_Ind1,t),D2Opt_State(E_Ind2,t),L)) > (V2(nextE_Ind2,t+1)+CtrlCost(D1,D2,L)) )
+                      %Note: Cost-to-go for given u&w (state at next time) is that at time t+1 in matrix V
+                      
+                      %For best combo, set cost-to-go to be that cost-to-go for the state
+                      CostE1_L(indL)=V1(nextE_Ind1,t+1); 
+                      CostE2_L(indL)=V2(nextE_Ind2,t+1); 
+                      %For best combo, set control to give that combo
+                      D1Opt_State(E_Ind1,t)=D1;  
+                      D2Opt_State(E_Ind2,t)=D2;  
+                      %^^ value of uOptState obtained here is UNUSED after value of CostX_W is found!!!
+                    end
                   end
                 end
               else
@@ -115,7 +120,7 @@ for t=(LAST_ITER-1):-1:1                %Start at 2nd-last iteration (time, t), 
         end
         
         %NOTE: IF NO PERTURBATION.... CostX_W should just hold cost of next state for the given value of u.
-        if(CostE1_L(indL)==INF)|(CostE2_L(indL)==INF) %If cannot go to any next state FOR GIVEN PERTURBATION w...
+        if(CostE1_L(indL)==INF)||(CostE2_L(indL)==INF) %If cannot go to any next state FOR GIVEN PERTURBATION w...
           fprintf('Got here. L=%d, E1=%d, E2=%d\n',L,E1,E2);
           %IGNORE possibility of such a perturbation. Perturbation w too large. No admissible next state
         else
