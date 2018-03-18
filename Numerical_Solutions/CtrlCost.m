89f0720c00491d971c7d7580fcc3c0384596a81d
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2018-03-18 12:56:28 -0400

    Small change: added excess discharging check

--- Numerical_Solutions/CtrlCost.m
+++ Numerical_Solutions/CtrlCost.m
@@ -2,9 +2,9 @@ function [ cost ] = CtrlCost( D1,D2,L ) % Input: D1(t), D2(t), L(t)
   global ALPHA_C; global ALPHA_D; global K;
   global MAX_DISCHARGE;
   
-  if(D1>MAX_DISCHARGE(1)||D2>MAX_DISCHARGE(2)||D1<0||D2<0) %If control is too high or low
+  if(D1>MAX_DISCHARGE(1)||D2>MAX_DISCHARGE(2)||D1<0||D2<0 ||(D1+D2)>L) %If control is too high or low
     cost=Inf;                                              %Make control inadmissible (infinite cost)
   else
-    cost=(1-ALPHA_D(1))*D1+K*(D1)^2+(1-ALPHA_D(2))*D2+(1-ALPHA_C(2))*(D1+D2-L); %Else calculate cost of control
+    cost=K*(D1)^2+((1-ALPHA_D(1))*D1+(1-ALPHA_D(2))*D2+(1-ALPHA_C(2))*(D1+D2-L)); %Else calculate cost of control
   end
 end
