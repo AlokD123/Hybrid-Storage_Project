b5149e0465782b5d748f0ae6e23283108d7e217c
AlokD123 <alokdeshpande@cmail.carleton.ca>
2018-07-04 22:28:02 -0400

    REMOVED INFEASIBLE CONTROLS in Exact LP
    Removed controls for which no states feasible. Corrected solution,

--- Numerical_Solutions/LP_sol_IHDP_v6.m
+++ Numerical_Solutions/LP_sol_IHDP_v6.m
@@ -4,7 +4,7 @@ clearvars -except X V;
 
 global E_MIN; global E_MAX;
 E_MIN=[0;0]; %Minimum energy to be stored (lower bound)
-E_MAX=[15;5]; %Maximum energy to be stored (upper bound)
+E_MAX=[5;4]; %Maximum energy to be stored (upper bound)
 
 %Solver tolerance
 tolerance=1e-6;
@@ -18,7 +18,7 @@ E2_INIT=E_MAX(2);
 %% Model setup
 global MAX_CHARGE; global MAX_DISCHARGE;
 MAX_CHARGE=[0;100]; %Maximum charging of the supercapacitor
-MAX_DISCHARGE=[15;5]; %Maximum discharging of the 1) battery and 2) supercap
+MAX_DISCHARGE=[5;4]; %Maximum discharging of the 1) battery and 2) supercap
 
 global MIN_LOAD;
 MIN_LOAD=0; %Minimum load expected
@@ -68,9 +68,11 @@ indL_Feas=[]; %Vector of feasible demands for ONE GIVEN combination of x and u
 Lmin_p=[]; %Vector of minimum loads required at high discharge (for given p)
 Lmin_offs_p=[]; %Vector of minimum load offsets for each E-state, to create CORRECT MAPPING in G matrix
 E_Ind_Mtx_p=[]; %Matrix of E_Ind_MtxALL values, but for EACH value of p
-P_fullmtx=[];
-c_state=[];
+P_fullmtx=[];   %Matrix of all probabilities
+c_state=[];     %Vector of state-relevance weightings
 State_NextStates=[];
+
+p_max=0;    %Maximum number of controls to consider
 
 %% PART A: SET UP MATRICES
 %For each possible control...
@@ -112,14 +114,7 @@ State_NextStates=[];
                         %Calculate the state these values of u and w will lead to, even if
                         %impossible...
                         [nextE1,nextE2]=optNextStateLimited(E1,E2,D1,D2,L);
-                        if(D1==MAX_DISCHARGE(1)||(round(nextE1)==0&&nextE1<=0)) %<---------------------------------------------------------------------------- SOL#2 for excess discharge: saturate state!!!!!!!!!!!!!!
-                           nextE1=0; 
-                        end
-                        
-                        boolFble=(nextE1<=E_MAX(1) && nextE1>=E_MIN(1))&&(nextE2<=E_MAX(2) && nextE2>=E_MIN(2))&&(~((D1+D2-L)<0||(D1+D2-L)>MAX_CHARGE(2)));
-                        
-                        State_NextStates=[State_NextStates;E1,E2,D1,D2,p,L,nextE1,nextE2,boolFble];
-                        
+
                         %If next state is amongst those achievable with a given perturbance....
                         if(nextE1<=E_MAX(1) && nextE1>=E_MIN(1))
                             if(nextE2<=E_MAX(2) && nextE2>=E_MIN(2))
@@ -182,14 +177,25 @@ State_NextStates=[];
                 end
             end
         end
+    
+    %If at least one feasible state, consider this control
+    if (~isempty(gVec_p))
+        %Store vector data in cell array
+        g{p}=gVec_p';
+        E_Ind_Vect{p}=E_Ind_Vect_p;
+        nextE_Ind_Vect{p}=nextE_Ind_Vect_p;
+        Lmin{p}=Lmin_p;
+        Lmin_offs{p}=Lmin_offs_p;
+        E_Ind_Mtx{p}=E_Ind_Mtx_p;
         
-    %Store vector data in cell array
-    g{p}=gVec_p';
-    E_Ind_Vect{p}=E_Ind_Vect_p;
-    nextE_Ind_Vect{p}=nextE_Ind_Vect_p;
-    Lmin{p}=Lmin_p;
-    Lmin_offs{p}=Lmin_offs_p;
-    E_Ind_Mtx{p}=E_Ind_Mtx_p;
+        %Continue testing next control
+        p_max=p_max+1;
+    else
+        %Else, IGNORE
+        %Also, STOP TESTING MORE CONTROLS (max feasible reached)
+        D1=MAX_DISCHARGE(1);
+        D2=MAX_DISCHARGE(2);
+    end
     
     %Reset matrices/vectors
     nextE_Ind_Vect_p=[];
@@ -222,7 +228,7 @@ State_NextStates=[];
   
   
   
-  for p=1:P1*P2
+  for p=1:p_max
     E_Ind_Vect_p=E_Ind_Vect{p};
     nextE_Ind_Vect_p=nextE_Ind_Vect{p};
     Lmin_offs_p=Lmin_offs{p};
@@ -286,7 +292,7 @@ State_NextStates=[];
   
   
   %STEP 9: Construct each F matrix
-  for p=1:P1*P2
+  for p=1:p_max
       aug_nextE_Ind_Vect_p=aug_nextE_Ind_Vect{p};
       %Index COLUMN of F matrix by ROW number of E_Ind_VectALL
       row=1; %Reset row being checked in E_Ind_VectALL to start when start on next E_Ind vector
@@ -325,7 +331,7 @@ State_NextStates=[];
   end
   
   %STEP 10: Construct each G matrix
-  for p=1:P1*P2
+  for p=1:p_max
       E_Ind_Vect_p=E_Ind_Vect{p};
       Lmin_offs_p=Lmin_offs{p};
       %Index COLUMN of G matrix by ROW number of E_Ind_VectALL
@@ -367,7 +373,7 @@ State_NextStates=[];
   end
   
   %If get empty g vector...
-  for p=1:P1*P2
+  for p=1:p_max
     if isempty(g{p})
         disp('ERROR!!'); %Error!!!
        g{p}=zeros(length(E_Ind_VectALL),1); %Set equal to zeros
@@ -377,7 +383,7 @@ State_NextStates=[];
   %% CORRECTED MATRICES (REMAINING infeasible states removed)
   %1) Coefficients
   Q=[];
-  for p=1:P1*P2
+  for p=1:p_max
     %Create full 'A' matrices for coefficients (A=G-alpha*PF)
     A{p}=G{p}-DISCOUNT*PF{p};
     %Adjoin A matrices to form Q
@@ -387,13 +393,13 @@ State_NextStates=[];
   %If empty columns in Q...
   if (~all(any(Q,1)))
       disp('ERROR!!!!!'); %ERROR
-     Q(:,~any(Q,1))=[]; %Remove, for now ]
+     Q(:,~any(Q,1))=[]; %Remove, for now
   end
   
   %2) Constants
   %Create full 'b' vector for constants
   b=[];
-  for p=1:P1*P2
+  for p=1:p_max
     b=[b;g{p}];
   end
   
@@ -438,7 +444,7 @@ State_NextStates=[];
     %->Augmented vectors for given values of p (i.e. like augmented versions of
     %E_Ind_VectALL, and subsets of E_MtxALL_Vect)
     E_MtxALL_Vect_subs={};
-    for p=1:P1*P2
+    for p=1:p_max
         E_Ind_Mtx_p=E_Ind_Mtx{p};
         E_Ind_Mtx_p(:,size(E_Ind_Mtx_p,2)+1:size(E_Ind_MtxALL,2))=0; %Pad with zeros on sid to make same size
         %Convert to vector
@@ -455,7 +461,7 @@ State_NextStates=[];
     %where some value is next value in for i=1:p-1 sumLen=sumLen+len(vecti) end optD(sumLen:sumLen+len(vectp))
     aug_optD_subP={};
     indOptD=1;
-    for p=1:P1*P2
+    for p=1:p_max
         aug_optD_subP_p=[];
         E_MtxALL_Vect_subs_p=E_MtxALL_Vect_subs{p};
        for i=1:length(E_MtxALL_Vect_subs_p)
@@ -473,26 +479,26 @@ State_NextStates=[];
     
     %3) Marginalise: sum vector components
     d_state=zeros(length(aug_optD_subP{1}),1); %Initialize
-    for(p=1:P1*P2)
+    for(p=1:p_max)
        d_state=d_state+aug_optD_subP{p}; %Sum over control values
     end
     
     %PART 2: Get stationary probabilities vector
     %Create augmented optD vector, for ALL states
     aug_optD=[];
-    for p=1:P1*P2
+    for p=1:p_max
         aug_optD=[aug_optD;aug_optD_subP{p}];
     end
-    %Create vector with vector d_state duplicated P1*P2 times and appended
+    %Create vector with vector d_state duplicated p_max times and appended
     %(to allow for dividing each probability by marginalized value)
-    dup_ones=ones(P1*P2,1);
+    dup_ones=ones(p_max,1);
     d_state_dup=kron(dup_ones,d_state);
     %Divide to get stationary probabilities vector for ALL states (augmented)
     aug_pi=aug_optD./d_state_dup;
     
     %Create augmented vector of all E_MtxALL_Vect_subs vectors
     aug_E_MtxALL_Vect=[];
-    for p=1:P1*P2
+    for p=1:p_max
         aug_E_MtxALL_Vect=[aug_E_MtxALL_Vect;E_MtxALL_Vect_subs{p}];
     end
     %Get stationary probabilities vector for ONLY feasible states (non-zero
