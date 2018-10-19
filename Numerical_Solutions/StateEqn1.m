ea03f517614e2be63d412a765befe9554bbd4671
AlokD123 <alokdeshpande@cmail.carleton.ca>
2018-10-18 22:22:11 -0400

    IMPORTANT: Added option to make more zero-energy states feasible
    Added option to set perfect efficiencies if allows for reducing E-state to 0.

--- Numerical_Solutions/StateEqn1.m
+++ Numerical_Solutions/StateEqn1.m
@@ -1,4 +1,3 @@
-function [ nextE_1 ] = StateEqn1( E1,uD1,beta ) % Input: E1(t), uD1(t), and value of beta to use
-  global ALPHA_D;
-  nextE_1=beta*E1-1/ALPHA_D(1)*uD1;
+function [ nextE_1 ] = StateEqn1( E1,uD1,beta,alpha ) % Input: E1(t), uD1(t), and values of efficiencies to use
+  nextE_1=beta*E1-1/alpha*uD1;
 end
--- Numerical_Solutions/optNextStateLimited.m
+++ Numerical_Solutions/optNextStateLimited.m
@@ -1,10 +1,11 @@
 %Calculate subsequent state accounting for no inherent storage loss when
 %dropping below lower bound of state
 function [ nextE1,nextE2 ] = optNextStateLimited( E1,E2,D1,D2,L ) % Input: state, controls, load
-    global E_MIN; global BETA;
-    newE1=StateEqn1(E1,D1,BETA(1));
+    global E_MIN; global BETA; global ALPHA_D; global ALPHA_C;
+    
+    newE1=StateEqn1(E1,D1,BETA(1),ALPHA_D(1));
     if(newE1<E_MIN(1))                      %If below lower bound...
-        if(StateEqn1(E1,D1,1)>=E_MIN(1))    %If would not be if with no inherent loss, ASSUME lossless
+        if(StateEqn1(E1,D1,1,ALPHA_D(1))>=E_MIN(1))  %If would not be if with no inherent loss, ASSUME lossless
             nextE1=E_MIN(1);                %In this case, will drop to lower bound
         else
             nextE1=newE1;                   %Else, doesn't matter
@@ -14,10 +15,14 @@ function [ nextE1,nextE2 ] = optNextStateLimited( E1,E2,D1,D2,L ) % Input: state
     end
     
     %Repeat for second storage
-    newE2=StateEqn2(E2,D1,D2,L,BETA(2));
+    newE2=StateEqn2(E2,D1,D2,L,BETA(2),ALPHA_C(2),ALPHA_D(2));
     if(newE2<E_MIN(2))
-        if(StateEqn2(E2,D1,D2,L,1)>=E_MIN(2))
+        if(StateEqn2(E2,D1,D2,L,1,ALPHA_C(2),ALPHA_D(2))>=E_MIN(2))
             nextE2=E_MIN(2);
+        %{
+        elseif(StateEqn2(E2,D1,D2,L,1,1,1)>=E_MIN(2))&&E2<=2
+            nextE2=E_MIN(2);
+        %}
         else
             nextE2=newE2;
         end
