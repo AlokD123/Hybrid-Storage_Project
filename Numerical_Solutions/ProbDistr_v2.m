85b2ada755ad813a315c58eecea0a76f3b147501
AlokD123 <alokdeshpande@cmail.carleton.ca>
2018-11-15 13:31:27 -0500

    FIXED probability generator for random demands
    Untested.

--- Numerical_Solutions/ProbDistr_v2.m
+++ Numerical_Solutions/ProbDistr_v2.m
@@ -1,23 +1,26 @@
-function [prob_nextE] = ProbDistr_v2(nnzProb_nextE,currState_indL,nextState_indLs_Vect,resL_Mult)
+function [prob_nextE] = ProbDistr_v2(nnzProb_nextE,currState_indL,nextState_indLs_Vect,boolSubSamp)
 %ProbDistr Transforms the input probabilities to a custom distribution
 %CUSTOM DISTRIBUTION: BINOMIAL
 %   Input: length of vector of uniform probabilities of next states (next demands);
 %           indL in single current state;
 %           vector of all possible indLs in next E-state;
-%           demand resolution multiplicative factor (natural number)
+%           indicator of subsampling probabilities for offline optimization
 %   Output: probabilities, with higher weight on staying near same
 %           state (demand)
 
-global epsilon3;
+global epsilon3; global resL_Mult;
+
+%Set demand resolution multiplicative factor (LARGE natural number)
+resL_Mult=10;
 
 %Transform #of next loads input by multiplicative factor to achieve higher
 %resolution of demand (during simulation, with continuous loads)
-nnzProb_nextE=resL_Mult*(nnzProb_nextE-1)+1;
+%nnzProb_nextE=resL_Mult*(nnzProb_nextE-1)+1;
 
 
 %Set number of "samples" controls number of possible next state loads (finite, non-negative)
 %Binomial PDF parameter n
-n=nnzProb_nextE-1; %One less because lowest binomial random variable is 0 instead of 1
+n=resL_Mult*nnzProb_nextE-1; %One less because lowest binomial random variable is 0 instead of 1
 
 %Small term to avoid all-none probability cases when n!=0 in binomial distribution
 epsP=(1/n)/2; %Change p by a term less than the resolution of p steps (1/n), so not making other load more probable
@@ -32,7 +35,8 @@ while isempty(d)
     diffL=diffL+1; %Step by discretization in load %<--------------------------------------- TO DO: make <1 if deltaL discretization is <1 (almost never)
 end
 
-%Transform d in the same way as nnzProb_nextE
+%Transform d by multiplicative factor to show higher
+%resolution of demand (during simulation, with continuous loads)
 d=resL_Mult*(d-1)+1;
 
 %Create vector containing BINOMIAL PDF PROBABILITIES <---------------------------------------------- DISTRIBUTION W/ BINOMIAL PDF!!!!
@@ -50,8 +54,17 @@ else
 end
 binomPDF_probs=binopdf(0:n,n,p);
 
+
 %Return custom probabilities
-prob_nextE=binomPDF_probs;
+if boolSubSamp
+    for i=1:nnzProb_nextE       %If subsampling, modify probabilities so only nnzProb_nextE ones sampled
+        sumProbSubSamp(i)=sum(binomPDF_probs((i-1)*resL_Mult+1:i*resL_Mult));
+    end
+    prob_nextE=sumProbSubSamp;
+else                            %Otherwise, return all
+    prob_nextE=binomPDF_probs;
+end
+
 
 end
 
