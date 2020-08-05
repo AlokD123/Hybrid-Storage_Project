3ba3463e5e21147489ea4d399b03d9b7c58f41ab
AlokD123 <alok.deshpande@mail.utoronto.ca>
2020-08-05 10:14:08 -0400

    Removed lots of old versions of scripts

--- Numerical_Solutions/ALP_full_test.m
+++
@@ -1,9 +0,0 @@
-%Full Approximate LP testing, including interpolation
-
-ApproxLP_sol_IHDP_v12;
-GetCtrlPolicy_OptQVals;
-PolicyCmpltnessEval_v2;
-sum(NumOptCtrls_2(:,4)) %To confirm each state has one single optimal control
-
-LP_IHDP_EvaluatePolicy_v2;
-VisualizeIHDP_LP_cost;
